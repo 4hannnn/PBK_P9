682bda696251d693265464d89a11e098140654b3
Alfi-B <alfeeetzy@gmail.com>
2025-05-11 21:16:15 +0700

    Codingan Component

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import ChildComponent from './components/ChildComponent.vue'
+
+export default {
+  components: {
+    ChildComponent
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Ini Komponen Induk</h1>
+    <ChildComponent />
+  </div>
+</template>
---
+++ my-vue-app/src/components/ChildComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Halo dari komponen anak!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
